773a5f47ae3cf10a562b0318f9c25e80e3f5add4
TungPT <tungpt@vnext.com.vn>
2017-08-23 11:10:33 +0700

    Add profilePaginator

--- profileManager.php
+++ profileManager.php
@@ -1,7 +1,11 @@
 <?php
+//require "vendor/autoload.php";
 include "Zend/Db.php";
 include "Zend/Db/Table.php";
 include "Zend/Registry.php";
+include "Zend/Paginator.php";
+include "Zend/Paginator/Adapter/Array.php";
+include "Zend/Paginator/Adapter/DbTableSelect.php";
 
 $dbAdapter = Zend_Db::factory('PDO_MYSQL', array(
     'host' => 'localhost',
@@ -99,3 +103,28 @@ foreach($profiles as $profile) {
     echo $id.' '. $fullname.' '.$address.PHP_EOL;
 }
 
+//Paginator
+//$paginator = new Zend_Paginator(new Zend_Paginator_Adapter_Array($profiles->toArray()));
+$profileSelect = $profileTable->select(); //Zend_Db_Table_Select  'select * from profile';
+
+$profilePaginator = new Zend_Paginator(new Zend_Paginator_Adapter_DbTableSelect($profileSelect));
+$profilePaginator->setItemCountPerPage(2);    
+
+echo PHP_EOL;
+for($page = 1; $page <= $profilePaginator->count();$page++){
+    echo "Page: $page";
+    echo PHP_EOL;
+    
+    $profilePaginator->setCurrentPageNumber($page);
+    foreach($profilePaginator as $profile){
+        $id = $profile->id;
+        $fullname = $profile->fullname;
+        $address = $profile->address;
+
+        echo $id.' '. $fullname.' '.$address.PHP_EOL;
+    }
+     echo PHP_EOL;
+    
+}
+
+var_dump($profilePaginator->count());
